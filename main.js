6bdc12b82a29d665d855d3e04ea50ac56bd1bd34
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2023-01-19 11:41:01 -0300

    :sparkles: Finish the BoardGame object

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ import "./src/styles/settings/colors.css"
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css"
 
-import CardGame from "./src/components/CardGame"
+import BoardGame from "./src/objects/BoardGame"
+import PlayerName from "./src/components/PlayerName"
 
 const $root = document.querySelector("#root")
-const $htmlCardGame = CardGame()
-$root.insertAdjacentHTML("beforeend", $htmlCardGame)
+const $htmlBoardGame = BoardGame(6)
+const $playerName1 = PlayerName('Player 1')
+const $playerName2 = PlayerName('Player 2')
+$root.insertAdjacentHTML("beforeend", $playerName1 + $playerName2 + $htmlBoardGame)
---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,12 @@
+import "./style.css"
+import CardGame from "../../components/CardGame"
+
+function BoardGame(amountCards = 2) {
+    const $htmlCardGame = CardGame()
+    const $htmlContent =  $htmlCardGame.repeat(amountCards)
+    return `<section class="board-game">
+        ${$htmlContent}
+    </section>`
+}
+
+export default BoardGame
